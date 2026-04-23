efa9ab35cbce0fedf0a0f17cf7ed787f86ef1322
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-23 09:16:31 +0545

    feat add protected view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({"message": f"Hello {request.user.email}, you are authenticated!"})
