931a407a03e80739d204ca4c4c2bb3ca6c4ae612
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-24 14:36:22 +0545

    feat update service viewset

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Category, Service
+from .serializers import CategorySerializer, ServiceSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.AllowAny]
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.query_params.get('category')
+        if category:
+            queryset = queryset.filter(category=category)
+        return queryset
