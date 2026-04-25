23b02a95072fb2e541c7b267fefea7e547152c06
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-25 19:35:41 +0545

    feat add booking urls

--- backend/marketplace/urls.py
+++ backend/marketplace/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ServiceViewSet
+from .views import CategoryViewSet, ServiceViewSet, BookingViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'services', ServiceViewSet)
+router.register(r'bookings', BookingViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
