14bb57f4b4ccb6a3776befbef522ad3b1518abcb
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-25 20:10:03 +0545

    fix urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
-    path('api/', include('marketplace.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/marketplace/', include('marketplace.urls')),
 ]
 
 from django.conf import settings
