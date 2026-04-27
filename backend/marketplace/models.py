2cfe401bc8ad4b0722ff7253b60f987aadef16c7
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-04-27 19:33:32 +0545

    feat add notification table

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -138,3 +138,16 @@ class Review(models.Model):
 
     def __str__(self):
         return f"Review by {self.user.email} for {self.service.title}"
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    message = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user.email}: {self.message[:20]}..."
