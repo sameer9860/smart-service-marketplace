6cd3d28342e35bae690982c2581c7cae84a6db53
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-05-14 18:29:55 +0545

    alter bid

--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -61,6 +61,13 @@ class Booking(models.Model):
         null=True,
         blank=True
     )
+    bid = models.ForeignKey(
+        'Bid',
+        on_delete=models.SET_NULL,
+        related_name='bookings',
+        null=True,
+        blank=True
+    )
     status = models.CharField(
         max_length=20, 
         choices=STATUS_CHOICES, 
